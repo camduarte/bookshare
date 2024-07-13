39a2d78009fa1de2a1a231678b46f6e14c4f80cb
A. J <adanj270594@gmail.com>
2024-07-13 17:39:43 -0400

    Poryecto inicial React

---
+++ client/src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './styles/reset.css';
+import { AppRouter } from './router/AppRouter';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AppRouter />
+    </BrowserRouter>
+  </React.StrictMode>
+);
