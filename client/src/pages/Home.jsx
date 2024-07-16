ab11a99bfcf93d5d2a3d264c5a290c6bf5b37ca6
A. J <adanj270594@gmail.com>
2024-07-16 09:47:37 -0400

    Componente button agregado

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import Button from '../components/ui/Button';
 
 export default function Home() {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      HomePage
+      <Button variant='default' size='default' disabled>
+        Default Button
+      </Button>
+      <Button variant='outline' size='sm'>
+        Outline Button
+      </Button>
+      <Button variant='ghost' size='sm'>
+        Ghost Button
+      </Button>
+    </div>
+  );
 }
