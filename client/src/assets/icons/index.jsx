68b1e0acff219a8766be4afa3caaff865243e79b
A. J <adanj270594@gmail.com>
2024-07-16 12:22:37 -0400

    Se agreagron iconos svg

---
+++ client/src/assets/icons/index.jsx
@@ -0,0 +1,171 @@
+export const SearchIcon = () => {
+  return (
+    <svg
+      width='17'
+      height='16'
+      viewBox='0 0 17 16'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M14.8246 14L11.958 11.1333M13.4913 7.33333C13.4913 10.2789 11.1035 12.6667 8.15801 12.6667C5.21249 12.6667 2.82468 10.2789 2.82468 7.33333C2.82468 4.38781 5.21249 2 8.15801 2C11.1035 2 13.4913 4.38781 13.4913 7.33333Z'
+        stroke='#71717A'
+        strokeWidth='1.33'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  );
+};
+
+export const ArrowRightIcon = () => {
+  return (
+    <svg
+      width='20'
+      height='20'
+      viewBox='0 0 20 20'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M4.16667 10H15.8333M15.8333 10L10 4.16667M15.8333 10L10 15.8333'
+        stroke='white'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  );
+};
+
+export const ChevronDownIcon = () => {
+  return (
+    <svg
+      width='20'
+      height='20'
+      viewBox='0 0 20 20'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M5 7.5L10 12.5L15 7.5'
+        stroke='#71717A'
+        strokeWidth='1.67'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  );
+};
+
+export const EditIcon = () => {
+  return (
+    <svg
+      width='21'
+      height='20'
+      viewBox='0 0 21 20'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <g clipPath='url(#clip0_138_765)'>
+        <path
+          d='M14.3667 2.49993C14.5856 2.28106 14.8454 2.10744 15.1314 1.98899C15.4173 1.87054 15.7238 1.80957 16.0334 1.80957C16.3429 1.80957 16.6494 1.87054 16.9354 1.98899C17.2213 2.10744 17.4812 2.28106 17.7 2.49993C17.9189 2.7188 18.0925 2.97863 18.211 3.2646C18.3294 3.55057 18.3904 3.85706 18.3904 4.16659C18.3904 4.47612 18.3294 4.78262 18.211 5.06859C18.0925 5.35455 17.9189 5.61439 17.7 5.83326L6.45003 17.0833L1.8667 18.3333L3.1167 13.7499L14.3667 2.49993Z'
+          stroke='#14AE5C'
+          strokeWidth='2'
+          strokeLinecap='round'
+          strokeLinejoin='round'
+        />
+      </g>
+      <defs>
+        <clipPath id='clip0_138_765'>
+          <rect
+            width='20'
+            height='20'
+            fill='white'
+            transform='translate(0.200073)'
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+
+export const PaperclipIcon = () => {
+  return (
+    <svg
+      width='21'
+      height='20'
+      viewBox='0 0 21 20'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M18.3667 9.20835L10.7084 16.8667C9.77017 17.8049 8.49769 18.332 7.17087 18.332C5.84405 18.332 4.57157 17.8049 3.63337 16.8667C2.69517 15.9285 2.16809 14.656 2.16809 13.3292C2.16809 12.0024 2.69517 10.7299 3.63337 9.79168L11.2917 2.13335C11.9172 1.50788 12.7655 1.15649 13.65 1.15649C14.5346 1.15649 15.3829 1.50788 16.0084 2.13335C16.6338 2.75882 16.9852 3.60713 16.9852 4.49168C16.9852 5.37623 16.6338 6.22455 16.0084 6.85001L8.3417 14.5083C8.02897 14.8211 7.60481 14.9968 7.16254 14.9968C6.72026 14.9968 6.29611 14.8211 5.98337 14.5083C5.67064 14.1956 5.49494 13.7715 5.49494 13.3292C5.49494 12.8869 5.67064 12.4627 5.98337 12.15L13.0584 5.08335'
+        stroke='#757575'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  );
+};
+
+export const PlusIcon = () => {
+  return (
+    <svg
+      width='20'
+      height='20'
+      viewBox='0 0 20 20'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M9.99999 4.16675V15.8334M4.16666 10.0001H15.8333'
+        stroke='#F3F3F3'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  );
+};
+
+export const TrashIcon = () => {
+  return (
+    <svg
+      width='21'
+      height='20'
+      viewBox='0 0 21 20'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M2.70007 5.00008H4.36674M4.36674 5.00008H17.7001M4.36674 5.00008L4.36674 16.6667C4.36674 17.1088 4.54233 17.5327 4.8549 17.8453C5.16746 18.1578 5.59138 18.3334 6.03341 18.3334H14.3667C14.8088 18.3334 15.2327 18.1578 15.5453 17.8453C15.8578 17.5327 16.0334 17.1088 16.0334 16.6667V5.00008M6.86674 5.00008V3.33341C6.86674 2.89139 7.04233 2.46746 7.3549 2.1549C7.66746 1.84234 8.09138 1.66675 8.53341 1.66675H11.8667C12.3088 1.66675 12.7327 1.84234 13.0453 2.1549C13.3578 2.46746 13.5334 2.89139 13.5334 3.33341V5.00008M8.53341 9.16675V14.1667M11.8667 9.16675V14.1667'
+        stroke='#EC221F'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  );
+};
+
+export const XIcon = () => {
+  return (
+    <svg
+      width='16'
+      height='16'
+      viewBox='0 0 16 16'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M12 4L4 12M4 4L12 12'
+        stroke='#18181B'
+        strokeWidth='1.33'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  );
+};
