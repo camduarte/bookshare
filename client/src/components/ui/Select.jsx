2d3b3069e7b8849dc1be796aff7c65495c1866a0
A. J <adanj270594@gmail.com>
2024-07-16 15:52:35 -0400

    documentación agregada

--- client/src/components/ui/Select.jsx
+++ client/src/components/ui/Select.jsx
@@ -1,6 +1,36 @@
 import React from 'react';
 import '../../styles/components/ui/select.css';
 
+/**
+ * Componente reutilizable select con label y manejo de errores.
+ *
+ * @component
+ * @param {object} props - Propiedades del componente.
+ * @param {string} [props.className=""] - Clases adicionales para el contenedor del select.
+ * @param {array} props.options - Opciones del select representadas como objetos {value, label}.
+ * @param {string} props.placeholder - Texto del placeholder del select.
+ * @param {string} props.name - Nombre del select y para asociar con el label.
+ * @param {string} props.label - Texto del label asociado al select.
+ * @param {boolean} props.required - Indica si el campo es obligatorio.
+ * @param {boolean} props.error - Indica si hay un error en el campo.
+ * @param {string} props.errorMessage - Mensaje de error a mostrar.
+ * @param {object} ref - Ref para acceder al elemento select.
+ * @param {...any} props - Otras props para pasar directamente al elemento select.
+ * 
+ * @returns {JSX.Element} - JSX que representa el componente Select.
+ * 
+ * @example
+ * // Uso básico del componente Select
+ * <Select
+ *   label='select'
+ *   options={options}
+ *   placeholder='Filtrar por género'
+ *   error
+ *   errorMessage='dasdasd'
+ * />
+ *
+ */
+
 const Select = React.forwardRef(
   (
     {
