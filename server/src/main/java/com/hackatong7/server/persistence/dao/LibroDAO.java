da8796c173d7413d29ed59b624da54ce06cc1d24
SantiTV <santiagotenjo12@hotmail.com>
2024-07-16 22:35:55 -0500

    Agrego archivos Entity y Interface de Usuario y Libro

---
+++ server/src/main/java/com/hackatong7/server/persistence/dao/LibroDAO.java
@@ -0,0 +1,22 @@
+package com.hackatong7.server.persistence.dao;
+
+import java.util.List;
+
+import com.hackatong7.server.domain.entity.Libro;
+
+public interface LibroDAO {
+    
+    Libro guardarLibro(Libro libro);
+
+    Libro actualizarLibro(Libro libro);
+
+    void eliminarLibro(Long id);
+
+    Libro obtenerLibroPorId(Long id);
+
+    List<Libro> listarLibros();
+
+    List<Libro> listarLibrosPorUsuarioId(Long usuarioId);
+
+
+}
