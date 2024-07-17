34cbc0dbfcf8a90c8fce2f64ff141ab6bc71be51
cduarte <duarte.camd@gmail.com>
2024-07-17 14:01:47 -0300

    Agrego esqueleto de la implementación del servicio de autenticación.

---
+++ server/src/main/java/com/hackatong7/server/application/service/impl/AuthServiceImpl.java
@@ -0,0 +1,15 @@
+package com.hackatong7.server.application.service.impl;
+
+import com.hackatong7.server.application.dto.RegistroUsuarioDTO;
+import com.hackatong7.server.application.service.AuthService;
+import com.hackatong7.server.domain.entity.Usuario;
+
+public class AuthServiceImpl implements AuthService {
+
+	@Override
+	public Usuario registrarUsuario(RegistroUsuarioDTO registroUsuarioDTO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
