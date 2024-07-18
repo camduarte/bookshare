40b7fbacfa4282976185480ebb4d1b2f379ad46e
cduarte <duarte.camd@gmail.com>
2024-07-18 11:04:56 -0300

    Manejo de errores en el servicio de registro de usuario

--- server/src/main/java/com/hackatong7/server/application/service/impl/AuthServiceImpl.java
+++ server/src/main/java/com/hackatong7/server/application/service/impl/AuthServiceImpl.java
@@ -1,12 +1,15 @@
 package com.hackatong7.server.application.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.hackatong7.server.application.dto.RegistroUsuarioDTO;
 import com.hackatong7.server.application.mapper.UsuarioMapper;
 import com.hackatong7.server.application.service.AuthService;
 import com.hackatong7.server.domain.entity.Usuario;
+import com.hackatong7.server.domain.exceptions.RegistroUsuarioException;
+import com.hackatong7.server.domain.exceptions.UsuarioExistenteException;
 import com.hackatong7.server.persistence.dao.UsuarioDAO;
 
 /**
@@ -27,6 +30,9 @@ public class AuthServiceImpl implements AuthService {
     //TODO: Agregar anotación @Autowired
     private UsuarioDAO usuarioDAO;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     /**
      * Registra un nuevo usuario en el sistema.
      * 
@@ -35,6 +41,17 @@ public class AuthServiceImpl implements AuthService {
      */
     @Override
     public Usuario registrarUsuario(RegistroUsuarioDTO registroUsuarioDTO) {
-        return usuarioDAO.guardarUsuario(UsuarioMapper.toEntity(registroUsuarioDTO));
+        if (usuarioDAO.findByCorreoElectronico(registroUsuarioDTO.getCorreo()) != null) {
+            throw new UsuarioExistenteException("El usuario con el correo electrónico " + registroUsuarioDTO.getCorreo() + " ya existe.");
+        }
+
+        try {
+            Usuario usuario = UsuarioMapper.toEntity(registroUsuarioDTO);
+            usuario.setContrasena(passwordEncoder.encode(registroUsuarioDTO.getContrasena()));
+            return usuarioDAO.guardarUsuario(usuario);
+        } catch (Exception e) {
+        	e.printStackTrace();
+            throw new RegistroUsuarioException("Error al registrar el usuario.");
+        }
     }
 }
