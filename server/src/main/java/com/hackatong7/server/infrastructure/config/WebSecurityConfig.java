141bac5d49befed904d0c79d996ac4b8831011df
cduarte <duarte.camd@gmail.com>
2024-07-20 18:58:58 -0300

    Probando cors

--- server/src/main/java/com/hackatong7/server/infrastructure/config/WebSecurityConfig.java
+++ server/src/main/java/com/hackatong7/server/infrastructure/config/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.hackatong7.server.infrastructure.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +14,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.hackatong7.server.infrastructure.filters.JwtAuthenticationFilter;
 
@@ -41,6 +46,16 @@ public class WebSecurityConfig {
             .cors(cors -> cors.disable());
 
         return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+    	CorsConfiguration configuration = new CorsConfiguration();
+    	configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+    	configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE"));
+    	UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    	source.registerCorsConfiguration("/**", configuration);
+    	return source;
     }
 
     @Bean
