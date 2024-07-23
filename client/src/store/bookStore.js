bdda2eecc442a17f2f0dc781b369d9acbb8dd2a1
A. J <adanj270594@gmail.com>
2024-07-22 22:12:34 -0400

    fix: se corrigieron varios bugs

--- client/src/store/bookStore.js
+++ client/src/store/bookStore.js
@@ -7,10 +7,12 @@ import { deleteBook } from '../services/deleteBook';
 import { getSearchData } from '../services/searchBook';
 import { getBookById } from '../services/authUser';
 import { getBooksByGenre } from '../services/filterBooks';
+import useAuthStore from './authStore';
 
 const useBookStore = create((set, get) => {
+  const getToken = () => useAuthStore.getState().token;
   const handleApiCall = async (apiCall, errorMessage) => {
-    const { token } = get();
+    const token = getToken();
     if (!token) {
       set({ error: 'No se encontró el token de autenticación' });
       return null;
@@ -36,12 +38,12 @@ const useBookStore = create((set, get) => {
     isLoading: false,
     error: null,
 
-    fetchBooks: async () => {
+    fetchAllBooks: async () => {
       const data = await handleApiCall(
         () => getAllBooks(get().token),
         'Error al cargar los libros'
       );
-      if (data) set({ allBooks: data });
+      if (data) set({ allBooks: data, filteredBooks: data });
     },
 
     fetchBookById: async (id) => {
@@ -105,12 +107,18 @@ const useBookStore = create((set, get) => {
       }
 
       const data = await handleApiCall(apiCall, errorMessage);
-      if (data) set({ filteredBooks: data });
+      if (data) {
+        set({ filteredBooks: data });
+        if (!params.searchTerm && !params.genre) set({ allBooks: data });
+      }
     },
 
     clearError: () => set({ error: null }),
 
-    clearFilteredBooks: () => set({ filteredBooks: [] }),
+    clearFilteredBooks: () => {
+      const { allBooks } = get();
+      set({ filteredBooks: allBooks });
+    },
   };
 });
 
